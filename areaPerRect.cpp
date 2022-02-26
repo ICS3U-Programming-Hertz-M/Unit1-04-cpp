58d19800c5c5dad02cd39afb8245a1e70ae970c2
Ubuntu <ubuntu@ip-172-31-28-1.ec2.internal>
2022-02-26 07:03:39 +0000

    Unit1-04-cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Hertz Mukiza  All rights reserved.
+//
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 8cm X 2cm.
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 8cm and width 2cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 8 * 2 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (8 + 2) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Hertz Mukiza  All rights reserved.
+//
+// Created by: Hertz mukiza
+// Date: Feb. 22, 2022
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "8+2=" <<(8+2)<< std::endl;
+    std::cout << "2*2=" <<(2*2)<< std::endl;
+    std::cout << "8-2=" <<(8-2)<< std::endl;
+    std::cout << "50+20/2=" <<(50+20/2)<< std::endl;
+    std::cout << "2+pow(2, 2)=" <<(2+pow(2, 2))<< std::endl;
+}
